78a5bcd541a05c447c79fa9ab1afae4f7c080bb4
c.richeux@gmail.com <gdM6ArcbCgUD>
2021-01-11 09:44:56 +0630

    Airtable Script V0.1

--- Airtable API/airtable_test.py
+++ Airtable API/airtable_test.py
@@ -6,30 +6,90 @@ from airtable import Airtable
 import mysql_module
 import mysql.connector
 
-#____________________________________________________________
-
+#Functions Airtable
 """
-airtable = Airtable('appCeehu9RgHJAPYY','Table 1',api_key='key2oMENkcz6kdrIy')
+def AirtableConnection ()
+  
+  airtable = Airtable('appIZ8SjK4PI6fDWc','Sites',api_key='key2oMENkcz6kdrIy')
 
-print(airtable.get_all(view='Name',sort='Notes'))
-print(airtable.get_all())
-"""
+def createAirtableLine ()
 
-##airtable.create('Table 1', {"Name":"Test import Python"})
-
-
-#____________________________________________________________
-#File name airtable_test.py
-#Fonctionne 
-"""
-airtable = Airtable('appIZ8SjK4PI6fDWc','Sites',api_key='key2oMENkcz6kdrIy')
-
-airtable.update_by_field('AP SITE ID', 'AYYN_0138', {'Site survey': 'Test script'})
+def AirtableConnection ()
 """
 
-#________________
-#MySQL Connection
+#Old script
+    import os
+    from pprint import pprint
+    from airtable import Airtable
+    #MySQL library
+    import mysql_module
+    import mysql.connector
 
+    mysqlConn = mysql.connector.connect(
+        host="192.168.1.40",
+        user="script.update",
+        passwd="zy6BjHr4k@sFa",
+        database="cos_python"
+        )
+
+    queryMySQL = """
+      SELECT
+        sites.`Site ID`,
+        sites.Zone
+      FROM sites
+    """
+
+    testList = mysql_module.getData (mysqlConn,queryMySQL)
+    print (testList)
+    def formatString(testList)
+      
+      stringFormat = testList
+      
+      return stringFormat
+
+    testOutput = formatString (testList)
+    print stringFormat
+
+
+    baseID = 'appIZ8SjK4PI6fDWc'
+    tableName = 'Sites'
+
+    lookupColumn = 'AP Site ID'
+    lookupData = 'AYYN_0138'
+
+    updateColumns = 'Site survey'
+    updateData = 'Test function script1'
+
+    updateAirtableLine (baseID,tableName,lookupColumn,lookupData,updateColumns,updateData)
+
+    #_____________________
+    #END
+
+  """
+  def updateAirtableLine (baseID,tableName,lookupColumn,lookupData,updateColumns,updateData)
+
+    #airtable = Airtable(baseID,tableName,api_key='key2oMENkcz6kdrIy')
+    
+    #airtable.update_by_field('AP SITE ID', 'AYYN_0138', {'Site survey': 'Test script'})
+    #airtable.update_by_field(lookupColumn, lookupData, {'Site survey': 'Test script'})
+    print (baseID,tableName,lookupColumn,lookupData,updateColumns,updateData)
+  """
+
+
+
+
+#___________________________
+#Script V1
+
+#Functions Airtable
+
+#Airtable library
+import os
+from pprint import pprint
+from airtable import Airtable
+#MySQL library
+import mysql_module
+import mysql.connector
 
 mysqlConn = mysql.connector.connect(
     host="192.168.1.40",
@@ -38,24 +98,62 @@ mysqlConn = mysql.connector.connect(
     database="cos_python"
     )
 
+mysqlConn2 = mysql.connector.connect(
+    host="192.168.1.40",
+    user="script.update",
+    passwd="zy6BjHr4k@sFa",
+    database="cos_python"
+    )
 
+#Query to python lists
 queryMySQL = """
 SELECT
   sites.`Site ID`,
-  sites.Zone
+  sites.`Site status`,
+  sites.`Site category`
 FROM sites
+WHERE sites.`Site ID` = 'ASMO_0176'
 """
-Test = mysql_module.getColumns (mysqlConn,queryMySQL)
-print(Test)
 
-Test2 = mysql_module.getData (mysqlConn,queryMySQL)
-print(Test2)
+ColumnSQL = mysql_module.getColumns (mysqlConn,queryMySQL)
+DataSQL = mysql_module.getData (mysqlConn2,queryMySQL)
 
-#Functions Airtable
-"""
-def updateAirtableLine ()
 
-def createAirtableLine ()
+lookupColumn = ColumnSQL[0]
+print (lookupColumn)
 
-def AirtableConnection ()
-"""
+DataLine = DataSQL[0]
+lookupData = DataLine [0]
+print (lookupData)
+
+
+stringUpdate = "{"
+
+for i in range(len(ColumnSQL)):
+
+   if i == 0: continue
+
+   incrColumnSQL = ColumnSQL[i]
+   incrLineSQL = DataLine[i]
+
+   updateItem = "'{}': '{}'"
+   updateItem = updateItem.format(incrColumnSQL,incrLineSQL)
+
+   stringUpdate = stringUpdate + "'"
+   stringUpdate = stringUpdate + incrColumnSQL + "' : '" + incrLineSQL +"'"
+
+   if i == len(ColumnSQL) -1: break
+   stringUpdate = stringUpdate + ','
+
+stringUpdate = stringUpdate + "}"
+#print (stringUpdate)
+
+execAirtable = "airtable.update_by_field('" + lookupColumn
+execAirtable = execAirtable + "','" + lookupData + "'," + stringUpdate + ")"
+#print(execAirtable)
+
+airtable = Airtable('appIZ8SjK4PI6fDWc','Sites',api_key='key2oMENkcz6kdrIy')
+
+exec(execAirtable)
+
+#airtable.update_by_field('SITE ID', 'AYYN_0138', {'Site survey': 'Test script','Label':'scriptons'})
